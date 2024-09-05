c88c0c8ffd252f7db258bfcb6ff3d36857aeed24
zayyehtut <zayyehtut.zy@gmail.com>
2024-09-05 22:45:23 +1000

    Add findByEmail method to StudentRepository.

--- student-service/src/main/java/com/university/student/domain/repository/StudentRepository.java
+++ student-service/src/main/java/com/university/student/domain/repository/StudentRepository.java
@@ -3,8 +3,9 @@ package com.university.student.domain.repository;
 import com.university.student.domain.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
- 
+    Optional<Student> findByEmail(String email);
 }
