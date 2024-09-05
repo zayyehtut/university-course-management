62576f27085ed5a71e3af48773cf735d1c185bdd
zayyehtut <zayyehtut.zy@gmail.com>
2024-09-05 22:44:03 +1000

    Enhance Student entity with table mapping, constructors, getters/setters, and equals/hashCode/toString methods.

--- student-service/src/main/java/com/university/student/domain/model/Student.java
+++ student-service/src/main/java/com/university/student/domain/model/Student.java
@@ -1,8 +1,10 @@
 package com.university.student.domain.model;
 
 import jakarta.persistence.*;
+import java.util.Objects;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
@@ -19,13 +21,85 @@ public class Student {
     @Column(nullable = false)
     private StudentType type;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "profile_id", referencedColumnName = "id")
     private StudentProfile profile;
 
-    // Constructors, getters, setters, etc.
-
     public enum StudentType {
         UNDERGRADUATE, POSTGRADUATE
+    }
+
+    // Constructors
+    public Student() {}
+
+    public Student(String name, String email, StudentType type) {
+        this.name = name;
+        this.email = email;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public StudentType getType() {
+        return type;
+    }
+
+    public void setType(StudentType type) {
+        this.type = type;
+    }
+
+    public StudentProfile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(StudentProfile profile) {
+        this.profile = profile;
+    }
+
+    // equals, hashCode, and toString methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", type=" + type +
+                '}';
     }
 }
