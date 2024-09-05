7ede2a0d8ea11fabf1be02661df27939cbe1fb75
zayyehtut <zayyehtut.zy@gmail.com>
2024-09-05 22:48:39 +1000

    Add StudentService interface defining core student management operations.

---
+++ student-service/src/main/java/com/university/student/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.university.student.service;
+
+import com.university.student.api.dto.StudentDTO;
+import com.university.student.api.dto.CreateStudentRequest;
+import com.university.student.api.dto.UpdateStudentRequest;
+
+import java.util.List;
+
+public interface StudentService {
+    StudentDTO createStudent(CreateStudentRequest request);
+    StudentDTO getStudentById(String id);
+    List<StudentDTO> getAllStudents();
+    StudentDTO updateStudent(String id, UpdateStudentRequest request);
+    void deleteStudent(String id);
+}
