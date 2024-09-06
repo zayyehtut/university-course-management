54a1eea7f1b9fed57c1c936a6b4309b0d6be0153
zayyehtut <zayyehtut.zy@gmail.com>
2024-09-06 19:25:00 +1000

    Refactor GradeRepository to add method for finding grades by enrollment ID

--- enrollment-service/src/main/java/com/university/enrollment/domain/repository/GradeRepository.java
+++ enrollment-service/src/main/java/com/university/enrollment/domain/repository/GradeRepository.java
@@ -1,10 +1,13 @@
+// File: enrollment-service/src/main/java/com/university/enrollment/domain/repository/GradeRepository.java
 package com.university.enrollment.domain.repository;
 
 import com.university.enrollment.domain.model.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, String> {
-  
+    List<Grade> findByEnrollmentId(String enrollmentId);
 }
