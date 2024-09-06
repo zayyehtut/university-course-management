ac2dc49dec645090583d1efa86ffb0b50f113c1c
zayyehtut <zayyehtut.zy@gmail.com>
2024-09-06 19:25:36 +1000

    Add EnrollmentService interface for managing enrollments and grades

---
+++ enrollment-service/src/main/java/com/university/enrollment/service/EnrollmentService.java
@@ -0,0 +1,20 @@
+package com.university.enrollment.service;
+
+import com.university.enrollment.api.dto.EnrollmentDTO;
+import com.university.enrollment.api.dto.GradeDTO;
+import com.university.enrollment.api.dto.CreateEnrollmentRequest;
+import com.university.enrollment.api.dto.UpdateGradeRequest;
+
+import java.util.List;
+
+public interface EnrollmentService {
+    EnrollmentDTO createEnrollment(CreateEnrollmentRequest request);
+    EnrollmentDTO getEnrollmentById(String id);
+    List<EnrollmentDTO> getAllEnrollments();
+    EnrollmentDTO updateEnrollmentStatus(String id, String status);
+    void deleteEnrollment(String id);
+    GradeDTO addGrade(String enrollmentId, UpdateGradeRequest request);
+    GradeDTO updateGrade(String gradeId, UpdateGradeRequest request);
+    GradeDTO getGradeById(String id);
+    List<GradeDTO> getGradesByEnrollmentId(String enrollmentId);
+}
